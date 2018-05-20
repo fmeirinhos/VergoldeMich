863994206229aa05b11167b63d18b3046f3b9e13
Meirinhos <francisco_meirinhos@hotmail.com>
2018-05-20 14:15:22 +0200

    Added stop_loss strategy

---
+++ vergoldemich/strategies/stop_loss.py
@@ -0,0 +1,62 @@
+from .signal import *
+from .strategy import *
+
+import logbook
+
+
+class StopLoss(Strategy):
+    """
+    A stop-loss order is an order placed with a broker to sell a security
+    when it reaches a certain price. Stop loss orders are designed to
+    limit an investor’s loss on a position in a security. Although most
+    investors associate a stop-loss order with a long position, it can also
+    protect a short position, in which case the security gets bought if it
+    trades above a defined price.
+
+    Source:
+    https://www.investopedia.com/terms/s/stop-lossorder.asp
+    """
+
+    params = dict(
+        target_profit=0.07,
+        stop_loss=0.05,
+        sell=0.10,
+        trail=False
+    )
+
+    def __init__(self, **kwargs):
+        """
+        Keyword Args:
+            sell (float) Price is x% less than the entry point
+            trail (Bool) trailing stop is a stop order that can be set at a defined percentage away from a security's current market price
+        """
+        super(StopLoss, self).__init__()
+
+        self.logger = logbook.Logger(self.__class__.__name__)
+
+        self.p.update(**kwargs)
+        self._check()
+
+    def _check(self):
+        if self.p.sell <= 0:
+            self.logger.error("Invalid sell percentage")
+            raise RuntimeError
+
+    def signal(self, market, context, data):
+        price = data.current(market, 'price')
+
+        # Get position
+        pos = context.portfolio.positions[market]
+
+        # Simple stop loss
+        if pos.amount > 0:
+
+            if price <= pos.cost_basis * (1 - self.p.stop_loss):
+                arg = 'StopLoss at {}'.format(self.p.sell)
+                return SIGNAL_SHORT, arg
+
+            elif price >= pos.cost_basis * (1 + self.p.target_profit):
+                arg = 'Target profit at {}'.format(self.p.target_profit)
+                return SIGNAL_SHORT, arg
+
+        return SIGNAL_NONE, ''
